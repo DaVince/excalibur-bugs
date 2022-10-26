2d989b9983152e921a13486e142cb328f9ca2516
Vincent Beers <vincent.beers@gmail.com>
2022-10-26 18:10:26 +0200

    Add example of action getting player stuck in fixed collision

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,29 @@
-import { Actor, Color, DisplayMode, Engine, Loader } from "excalibur";
+import { Actor, CollisionType, Color, DisplayMode, Engine, Loader, vec } from "excalibur";
 import { Player } from "./player";
 import { Resources } from "./resources";
 
 class Game extends Engine {
     constructor() {
-      super({width: 800, height: 600});
+      super({width: 800, height: 600, maxFps: 60});
     }
     initialize() {
-      
+
       const player = new Player();
       this.add(player);
+
+      const box = new Actor({
+        pos: vec(300, 300),
+        color: Color.Red,
+        width: 100,
+        height: 100,
+        collisionType: CollisionType.Fixed
+      });
+      this.add(box);
 
       const loader = new Loader([Resources.Sword]);
       this.start(loader);
     }
   }
-  
+
   export const game = new Game();
   game.initialize();
--- src/player.ts
+++ src/player.ts
@@ -1,19 +1,43 @@
-import { Actor, Color, vec } from "excalibur";
+import { Actor, CollisionType, Color, Engine, Input, vec } from "excalibur";
 import { Resources } from "./resources";
 
 export class Player extends Actor {
+  engine: Engine;
+
   constructor() {
     super({
       pos: vec(150, 150),
       width: 100,
-      height: 100
+      height: 100,
+      collisionType: CollisionType.Active
     });
   }
 
   onInitialize() {
     this.graphics.add(Resources.Sword.toSprite());
-    this.on('pointerup', () => {
-      alert('yo');
-    });
+  }
+
+  public onPreUpdate(_engine: Engine, _delta: number): void {
+    this.engine = _engine;
+
+    // Handle input
+    if (_engine.input.keyboard.isHeld(Input.Keys.Up)) {
+      this.move(0, -32);
+    }
+    if (_engine.input.keyboard.isHeld(Input.Keys.Down)) {
+      this.move(0, 32);
+    }
+    if (_engine.input.keyboard.isHeld(Input.Keys.Left)) {
+      this.move(-32, 0);
+    }
+    if (_engine.input.keyboard.isHeld(Input.Keys.Right)) {
+      this.move(32, 0);
+    }
+  }
+
+  public move(x: number, y: number) {
+    if (this.actions.getQueue().isComplete()) {
+      this.actions.moveBy(vec(x, y), this.engine.maxFps * 8).delay(50);
+    }
   }
 }
